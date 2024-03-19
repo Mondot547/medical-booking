85389a6aef4c42f0a9a2e381336f1286baf20a8c
Mondot547 <alex.mondot.mail.pro@gmail.com>
2024-03-19 18:45:38 +0100

    mise en place environnement frontend

---
+++ frontend/src/views/Home.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+
+const Home = () => {
+    return (
+        <>
+            <header>
+                <NavBar />
+                <h1>hi ! this is my home page !</h1>
+            </header>
+        </>
+    );
+};
+
+export default Home;
